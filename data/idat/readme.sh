09529853ce25963cca67054389220fd79d0e3077
vartikabisht6197 <vartikabisht6197@gmail.com>
2025-08-22 13:48:29 +0200

    Add example IDAT files and metadata for DNA methylation samples for checking the processing of the script relatively quickly.

---
+++ data/idat/readme.sh
@@ -0,0 +1,16 @@
+# Peripheral blood mononuclear cells (PBMCs) were isolated by density gradient centrifucation (Ficoll) 
+# of 4 patients diagnosed with Post-PV-MF, 4 patients diagnosed with PV and 4 healthy donors.
+mkdir 9374343009 ; cd 9374343009
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM8533627
+# Sex: Female
+# age: 72
+# tissue: PBMC
+# PBMC
+wget "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8533nnn/GSM8533627/suppl/GSM8533627%5F9374343009%5FR04C02%5FGrn.idat.gz"
+wget "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8533nnn/GSM8533627/suppl/GSM8533627%5F9374343009%5FR04C02%5FRed.idat.gz"
+# https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSM8533629
+# Sex: Male
+# age: 77
+# tissue: PBMC
+wget "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8533nnn/GSM8533629/suppl/GSM8533629%5F9374343009%5FR06C02%5FGrn.idat.gz"
+wget "https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM8533nnn/GSM8533629/suppl/GSM8533629%5F9374343009%5FR06C02%5FRed.idat.gz"
