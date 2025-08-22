214bd18a82a2c4851ae727a19cdc04a309972b90
vartikabisht6197 <vartikabisht6197@gmail.com>
2025-08-22 13:47:10 +0200

    Add configuration script for DNA methylation project with platform and metadata settings

---
+++ config.sh
@@ -0,0 +1,7 @@
+PLATFORM=HM450
+ASSEMBLY=hg19
+MANIFEST_KEY=data/manifest.annotation.key.csv
+METADATASHEET=data/metadatasheet.csv
+OUTDIR=${PROJECT}/projects/VB250630_piama_dna_methylation/VB250818
+IDATDIR=data/idat
+LOGDIR=${OUTDIR}/log
